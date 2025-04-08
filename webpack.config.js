e0e65636421524ab998ccda0a9600281a1423eb3
Mehul Gediya <mehul@zealousweb.com>
2025-04-08 17:53:36 +0530

    commenting code

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+// Webpack config (ES6 Compatible) with full comments
+
 import path from "path";
 import { fileURLToPath } from "node:url";
 import TerserPlugin from "terser-webpack-plugin";
@@ -7,21 +9,60 @@ import { WebpackManifestPlugin } from "webpack-manifest-plugin";
 import StylelintPlugin from "stylelint-webpack-plugin";
 import ESLintPlugin from "eslint-webpack-plugin";
 
+// Get current file path and directory
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
+// Detect if we're in development mode
 const isDev = process.argv.includes("--watch");
+
+// Base plugins always applied
+const plugins = [
+  new MiniCssExtractPlugin({
+    filename: "css/[name].[contenthash].css",
+    chunkFilename: "css/[id].[contenthash].css",
+  }),
+  new WebpackManifestPlugin({
+    publicPath: "/",     // Ensure proper public path
+    basePath: "",        // No double prefix
+  }),
+];
+
+// Add linting plugins only for production builds
+if (!isDev) {
+  plugins.push(
+    new StylelintPlugin({
+      configFile: path.resolve(dirname, "config/stylelint.config.js"),
+      context: "source/scss",
+      files: "**/style.scss",
+      failOnError: true,
+      fix: false,
+    }),
+    new ESLintPlugin({
+      context: "source/js",
+      extensions: ["js"],
+      overrideConfigFile: path.resolve(dirname, "config/eslint.config.js"),
+      failOnError: true,
+      fix: false,
+    })
+  );
+}
+
+// Export Webpack config object
 export default {
   mode: isDev ? "development" : "production",
+
   entry: {
     main: ["./source/js/script.js", "./source/scss/style.scss"],
   },
+
   output: {
     path: path.resolve(dirname, "public"),
     filename: "js/[name].[contenthash].js",
     clean: true,
     publicPath: "/",
   },
+
   module: {
     rules: [
       {
@@ -39,41 +80,25 @@ export default {
         test: /\.(scss|css)$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|avif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext][query]",
+        },
+      },
     ],
   },
+
   resolve: {
-    extensions: ['.scss'],
+    extensions: [".scss"],
     alias: {
-      '@scss': path.resolve(dirname, 'source/scss'),
+      "@scss": path.resolve(dirname, "source/scss"),
     },
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash].css",
-      chunkFilename: "css/[id].[contenthash].css",
-    }),
-    new WebpackManifestPlugin({
-      publicPath: "public",
-      basePath: "public",
-    }),
-    // lint will run on every build, so we need to check if we are in dev mode
-    ...(!isDev ? [
-        new StylelintPlugin({
-          configFile: path.resolve(dirname, "config/stylelint.config.js"),
-          context: "source/scss",
-          files: "**/style.scss",
-          failOnError: true,
-          fix: false
-        }),
-        new ESLintPlugin({
-          context: "source/js",
-          extensions: ["js"],
-          overrideConfigFile: path.resolve(dirname, "config/eslint.config.js"),
-          failOnError: true,
-          fix: false,
-        })
-    ]:[]),
-  ],
+
+  plugins,
+
   optimization: {
     minimize: !isDev,
     minimizer: [
@@ -87,8 +112,11 @@ export default {
       new CssMinimizerPlugin(),
     ],
   },
+
   devtool: isDev ? "source-map" : false,
+
   watch: isDev,
+
   stats: {
     colors: true,
     modules: false,
@@ -96,9 +124,10 @@ export default {
     chunks: false,
     chunkModules: false,
   },
+
   performance: {
     hints: isDev ? false : "warning",
-    maxEntrypointSize: 512000, // 500kb
-    maxAssetSize: 512000, // 500kb
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
   },
 };
